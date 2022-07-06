d481c4cb0621993edfaba0f1a3c8ba0658205375
SamiAtha <105737131+SamiAtha@users.noreply.github.com>
2022-07-07 02:33:16 +0700

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Simple TODO</title><link href>
+</head>
+<body>
+    <div class="container ny-4>
+    <div class="form-group">
+        <label for="input-nama">nama</label>
+        <input type="text" id="name" name="name" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="input-description">description</label>
+        <input type="text" id="name" name="name" class="form-control">
+    </form>
+    </div>
+
+    <br>
+
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Desctription</th>
+                    <th>Action</th>
+</tr>
+<thead>
+</Table>
+</div>
+</body>
+</html>
